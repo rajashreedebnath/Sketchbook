2fa9a47ab7c29377fc4046934952fbb1f3228edb
Rajashree <rajashreedebnath29@gmail.com>
2025-07-15 13:05:54 +0530

    Update useCanvasSetup.js

--- src/hooks/useCanvasSetup.js
+++ src/hooks/useCanvasSetup.js
@@ -3,11 +3,26 @@ import { useEffect } from "react";
 export default function useCanvasSetup(canvasRef, stack) {
   useEffect(() => {
     const canvas = canvasRef.current;
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
     const ctx = canvas.getContext("2d");
 
+    const resizeCanvas = () => {
+      const imageData = canvas.toDataURL();
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      const img = new Image();
+      img.onload = () => {
+        ctx.drawImage(img, 0, 0);
+      };
+      img.src = imageData;
+    };
+
+    const saveState = () => {
+      const data = canvas.toDataURL();
+      stack.current.push(data);
+      localStorage.setItem("latestCanvas", data);
+    };
+
+    
     const saved = localStorage.getItem("latestCanvas");
     if (saved) {
       const img = new Image();
@@ -17,26 +32,12 @@ export default function useCanvasSetup(canvasRef, stack) {
       };
       img.src = saved;
     } else {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
       ctx.fillStyle = "#ffffff";
       ctx.fillRect(0, 0, canvas.width, canvas.height);
       saveState();
     }
-
-    const saveState = () => {
-      const data = canvas.toDataURL();
-      stack.current.push(data);
-      localStorage.setItem("latestCanvas", data);
-    };
-
-    const resizeCanvas = () => {
-      const imageData = canvas.toDataURL();
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
-      const ctx = canvas.getContext("2d");
-      const img = new Image();
-      img.onload = () => ctx.drawImage(img, 0, 0);
-      img.src = imageData;
-    };
 
     window.addEventListener("resize", resizeCanvas);
     return () => window.removeEventListener("resize", resizeCanvas);
